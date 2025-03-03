bf38b4d83dbc6a76f4dcdf2ca21e220efa40f5f6
guanchuan1994 <120831767@qq.com>
2025-03-03 06:10:21 +0000

    update linklist

--- 链表/double-linked-list-fundamental.js
+++ 链表/double-linked-list-fundamental.js
@@ -6,7 +6,7 @@ const DoubleLinkedList = function (val) {
     this.val = val
 }
 const array = [1,2,3,4,5,6]
-
+const pos = 4
 const createDoubleLinkedList = () => {
     const head = new DoubleLinkedList(array[0])
     let node = head
@@ -40,6 +40,7 @@ const iterate = (dbll, cb) => {
         cb(tail.val)
         tail = tail.prev
     }
+
 }
 
 iterate(doubleLinkedList, (val) => console.log('doubleIterate',val))
@@ -53,7 +54,37 @@ newHead.next = addFromHeadLL;
 iterate(newHead, (val) => console.log('headInsert',val))
 
 // 2. 从中
+const addFromMiddle = createDoubleLinkedList();
+const newMiddle = new DoubleLinkedList(222)
+let node = addFromMiddle
+for(let i = 1 ; i < pos - 1; i++) {
+    node = node.next
+}
+newMiddle.next = node.next
+newMiddle.prev = node;
+node.next.prev = newMiddle;
+node.next = newMiddle;
+iterate(addFromMiddle, val => console.log('middleInsert', val))
+
 // 3. 从尾
 
+const addFromLast = createDoubleLinkedList();
 
+const newTail = new DoubleLinkedList(333);
+
+node = addFromLast
+for(; node.next != null ; node = node.next) {}
+newTail.prev = node;
+node.next = newTail;
+iterate(addFromLast, (val) => console.log('lastInsert',val))
+
+// 删
+// 1. 从头
+const deleteFromHead = createDoubleLinkedList();
+deleteFromHead.next.prev = null
+console.log('deleteHead', deleteFromHead.next)
+
+// 2. 从中
+
+// 3. 从尾
 export default {}
