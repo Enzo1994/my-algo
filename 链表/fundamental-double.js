2ebd8e69640be1d4f6cbf7cad8219ad66ffd0b50
guanchuan1994 <120831767@qq.com>
2025-03-03 08:40:52 +0000

    环形数组

--- 链表/fundamental-double.js
+++ 链表/fundamental-double.js
@@ -26,27 +26,28 @@ console.log(doubleLinkedList)
 // 遍历
 const iterate = (dbll, cb) => {
     let iterateNode = dbll
-    
     let tail = null;
+
     // 1. 从头
     while (iterateNode != null) {
         cb(iterateNode.val)
         tail = iterateNode;
         iterateNode = iterateNode.next
     }
-    console.log('reverse-------------------')
+    console.log('reverse-------------------', tail.val, iterateNode)
+    
     // 2. 从尾
     while(tail != null) {
         cb(tail.val)
         tail = tail.prev
     }
-
 }
 
 iterate(doubleLinkedList, (val) => console.log('doubleIterate',val))
 
 // 增
 // 1. 从头
+
 let addFromHeadLL = deepClone(doubleLinkedList)
 const newHead = new DoubleLinkedList(111)
 addFromHeadLL.prev = newHead;
@@ -54,6 +55,7 @@ newHead.next = addFromHeadLL;
 iterate(newHead, (val) => console.log('headInsert',val))
 
 // 2. 从中
+
 const addFromMiddle = createDoubleLinkedList();
 const newMiddle = new DoubleLinkedList(222)
 let node = addFromMiddle
@@ -66,25 +68,60 @@ node.next.prev = newMiddle;
 node.next = newMiddle;
 iterate(addFromMiddle, val => console.log('middleInsert', val))
 
+
 // 3. 从尾
 
 const addFromLast = createDoubleLinkedList();
-
 const newTail = new DoubleLinkedList(333);
-
 node = addFromLast
-for(; node.next != null ; node = node.next) {}
+
+// for(; node.next != null ; node = node.next) {}
+while(node.next != null) {
+    // 最后结果是倒数第二个
+    node = node.next
+}
 newTail.prev = node;
 node.next = newTail;
 iterate(addFromLast, (val) => console.log('lastInsert',val))
 
+
 // 删
 // 1. 从头
+
 const deleteFromHead = createDoubleLinkedList();
-deleteFromHead.next.prev = null
+if(deleteFromHead.next) {
+    deleteFromHead.next.prev = null
+}
 console.log('deleteHead', deleteFromHead.next)
+
 
 // 2. 从中
 
+const deleteFromMiddle = createDoubleLinkedList();
+node = deleteFromMiddle;
+
+for(let i = 1; i < pos - 1; i++) {
+    node = node.next
+}
+const next = node.next.next
+if (next) {
+    next.prev = node;
+}
+node.next = next
+iterate(deleteFromMiddle, (val) => console.log('middleDelete',val))
+
+
 // 3. 从尾
+
+const deleteFromLast = createDoubleLinkedList();
+node = deleteFromLast;
+if (node.next == null) {
+    while(node.next.next != null) {
+        node = node.next
+    }
+    node.next = null;
+}
+
+iterate(deleteFromLast, (val) => console.log('lastDelete',val))
+
 export default {}
