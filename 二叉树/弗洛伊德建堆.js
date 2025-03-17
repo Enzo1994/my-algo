2d1527627ca6aa1a876400407095031036bfa69c
guanchuan1994 <120831767@qq.com>
2025-03-17 04:09:10 +0000

    floyd's Heap Construction Done

--- 二叉树/弗洛伊德建堆.js
+++ 二叉树/弗洛伊德建堆.js
@@ -1,8 +1,39 @@
 
 const heapify = (arr, size, parentNodeIdx) => {
 
+    const leftChild = parentNodeIdx * 2 + 1
+    const rightChild = parentNodeIdx * 2 + 2
+
+    let largest = parentNodeIdx;
+
+    if (leftChild < size && arr[leftChild] > arr[parentNodeIdx] ) {
+        largest = leftChild
+    }
+
+    if (rightChild < size && arr[rightChild] > arr[parentNodeIdx]) {
+        largest = rightChild
+    }
+
+    if (largest !== parentNodeIdx) { 
+        [arr[largest], arr[parentNodeIdx]] = [arr[parentNodeIdx], arr[largest]]
+        heapify(arr, size, largest)
+    }
 }
+
+const arr = [4, 10, 3, 5, 1];
 
 const buildMaxHeap = () => {
 
+    const size = arr.length
+    const lastNonLeafNodeIdx = (arr.length - 1 >> 1) - 1
+
+    console.log(lastNonLeafNodeIdx)
+
+    for (let i = lastNonLeafNodeIdx; i >= 0; i--) {
+        heapify(arr, size, i)
+    }
+
+    console.log(arr)
 }
+
+buildMaxHeap()
